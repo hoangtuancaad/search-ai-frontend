18b6ece4187fa4fbc8d1b666cddf14e40357be85
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2024-05-24 10:28:18 +0700

    First commit

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
+
+export default [
+    {
+        files: ["src/**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+
+        languageOptions: {
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+            globals: { ...globals.browser },
+        },
+
+        rules: {
+            semi: ["error", "always"],
+            quotes: ["error", "double"],
+
+            "no-unused-vars": "error",
+            // "no-undef": "error",
+        },
+
+        ignores: ["**/node_modules/**", "**/dist/**", "**/public/**", "**src/**/*.scss"],
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...fixupConfigRules(pluginReactConfig),
+];
